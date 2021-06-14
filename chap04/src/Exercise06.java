09bc0e0785e33b59140a3b28741684e4719fa871
Ctdlog <73215539+Choi-HW980402@users.noreply.github.com>
2021-06-14 16:05:51 +0900

    ~5장. 참조 타입 p.161

--- chap04/src/Exercise06.java
+++ chap04/src/Exercise06.java
@@ -2,10 +2,9 @@
 public class Exercise06 {
 
 	public static void main(String[] args) {
-		int i;
 		String str = "*";
 		
-		for(i=1; i<=5; i++) {
+		for(int i=1; i<=5; i++) {
 			String repeated = str.repeat(i);
 			System.out.println(repeated);
 		}
