2436f2cd9666755fdb5800329f99017a1c8e40bf
Ctdlog <73215539+Choi-HW980402@users.noreply.github.com>
2021-06-22 20:35:40 +0900

    ~6.10.3 정적 초기화 블록

---
+++ chap06/src/CarExample4.java
@@ -0,0 +1,12 @@
+
+public class CarExample4 {
+
+	public static void main(String[] args) {
+		Car4 myCar = new Car4("Æ÷¸£½¦");
+		Car4 yourCar = new Car4("º¥Ã÷");
+		
+		myCar.run();
+		yourCar.run();
+	}
+
+}
